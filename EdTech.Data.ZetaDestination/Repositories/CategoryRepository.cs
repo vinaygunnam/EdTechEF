ed749b28a019b09f3a81bd3211e6c343f71fc4fd
Vinay Gunnam <vinaygunnam@live.com>
2020-09-01 22:18:20 -0500

    Upsert

---
+++ EdTech.Data.ZetaDestination/Repositories/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EdTech.Data.Common;
+
+namespace EdTech.Data.ZetaDestination.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category, NWDestinationEntities>
+    {
+        public CategoryRepository(NWDestinationEntities entities) : base(entities)
+        {
+        }
+
+        public void Upsert(IEnumerable<Category> collection)
+        {
+            if (collection == null || collection.Count() == 0) return;
+
+            var primaryKeys = collection.Select(c => c.CategoryID);
+            var existingRecords = this.SelectAll().Where(c => primaryKeys.Contains(c.CategoryID)).ToList();
+
+            collection.ToList().ForEach(categoryToBeUpserted =>
+            {
+                var match = existingRecords.SingleOrDefault(c => c.CategoryID == categoryToBeUpserted.CategoryID);
+                if (match == null)
+                {
+                    // insert the record
+                    Insert(categoryToBeUpserted);
+                }
+                else
+                {
+                    Update(categoryToBeUpserted);
+                }
+            });
+        }
+    }
+}
